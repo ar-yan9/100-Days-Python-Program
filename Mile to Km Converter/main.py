07155a740489a3e360f00b4dff622008b6e1c6df
ar-yan9 <aryankumar6360@gmail.com>
2026-05-13 11:59:14 +0530

    Add Mile to Km Converter project

---
+++ Mile to Km Converter/main.py
@@ -0,0 +1,30 @@
+from tkinter import *
+
+def calculate():
+    mile = float(miles_input.get())
+    km = mile * 1.60934
+    kilometer_result_label.config(text=f"{round(km, 2)}")
+
+window = Tk()
+window.title("Mile to Km Converter")
+window.config(padx=20, pady=20)
+
+miles_input = Entry(width=10)
+miles_input.grid(column=1, row=0)
+
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
+
+equal_label = Label(text="is equal to")
+equal_label.grid(column=0, row=1)
+
+kilometer_result_label = Label(text="0")
+kilometer_result_label.grid(column=1, row=1)
+
+km_label = Label(text="Km")
+km_label.grid(column=2, row=1)
+
+calculate_button = Button(text="Calculate", command=calculate)
+calculate_button.grid(column=1, row=2)
+
+window.mainloop()
